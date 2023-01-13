2c5065d55c762318becde824255d695370e25607
theshriramgupta <71961083+theshriramgupta@users.noreply.github.com>
2023-01-13 16:09:30 +0530

    Create whether-a-number-is-negative-or-positive.cpp

---
+++ whether-a-number-is-negative-or-positive.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float num;
+    
+    cin >> num;
+    
+    if(num >= 0.0){
+        cout << "The number inputed is positive";
+    }
+    else{
+        cout << "The number inputed is negative";
+    }
+    return 0;
+}
